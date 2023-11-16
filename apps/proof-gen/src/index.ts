51f51ba7881372c74282e4252187e1ed42d32214
Yoga2015 <yoga3333@126.com>
2023-11-16 22:53:45 +0800

    Determine whether the current node is the primary node

--- apps/proof-gen/src/index.ts
+++ apps/proof-gen/src/index.ts
@@ -75,7 +75,8 @@ const proof_generation_init = async () => {
         let subProcessCordinator = await createSubProcesses(config.subProcessCnt);
         // start web server in worker thread
         bootWebServerThread(subProcessCordinator);
-    } else {// sub processes:
+    } else {
+        // sub processes:
         await initWorker();
     }
 }
