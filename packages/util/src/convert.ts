2bec3ea31b40b69b678cff12b658405f6db129b7
Yoga2015 <yoga3333@126.com>
2023-11-20 22:51:38 +0800

    Extract fields and scalars from ProviderSignature object

--- packages/util/src/convert.ts
+++ packages/util/src/convert.ts
@@ -1,14 +1,6 @@
 import { Field, Scalar, Signature } from 'snarkyjs';
 import { ProviderSignature } from './mina_provider';
 
-// export function convertProviderSignatureToSignature(
-//   sign: ProviderSignature
-// ): Signature {
-//   return Signature.fromObject({
-//     r: Field(sign.field),
-//     s: Scalar.fromJSON(sign.scalar),
-//   });
-// }
 
 export function convertProviderSignatureToSignature(
   sign: ProviderSignature
