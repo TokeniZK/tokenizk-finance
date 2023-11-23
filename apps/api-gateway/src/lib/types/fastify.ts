00f40894222d66d6f353b60cb590d9916e2f0123
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-11-23 23:10:59 +0800

    add reqId to track each httpreq

--- apps/api-gateway/src/lib/types/fastify.ts
+++ apps/api-gateway/src/lib/types/fastify.ts
@@ -12,7 +12,8 @@ export type RequestHandler<
 > = (
     req: FastifyRequest<{
         Body: Body,
-        Params: Params
+        Params: Params,
+        reqId: string,
     }>,
     res: FastifyReply
 ) => any
