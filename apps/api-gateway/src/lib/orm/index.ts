94dc713cf0a2becd812116439fab7fa66e5c1533
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-11-24 23:26:35 +0800

    add pg orm for archive node

--- apps/api-gateway/src/lib/orm/index.ts
+++ apps/api-gateway/src/lib/orm/index.ts
@@ -23,3 +23,25 @@ export const initORM = async (connectionOverrides?: Partial<MysqlConnectionOptio
         return logger.info(error)
     }
 };
+export const initORMPg = async (connectionOverrides?: Partial<MysqlConnectionOptions>) => {
+    logger.info('### INFO: Creating PG Connection for typeORM')
+    try {
+        const connection = await createConnection(<MysqlConnectionOptions>{
+            ...config.typeORM,
+            // 【error1】 .export const initORM = async (connectionOverrides?: Partial<MysqlConnectionOptions>) => {
+    logger.info('### INFO: Creating Mysql Connection for typeORM')
+    try {
+        const connection = await createConnection(<MysqlConnectionOptions>{
+            ...config.typeORMPg,
+            // 【error1】 ...entities
+            // 【error2】 entities: [...entities]
+            entities: [],
+            ...connectionOverrides,
+        });
+        logger.info('### INFO: Connection Established！')
+        return pgconnection
+    } catch (error) {
+        return logger.info(error)
+    }
+};
+};
