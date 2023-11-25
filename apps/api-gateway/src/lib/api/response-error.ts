f027893b3827785b477b023564a11cb84cc1de47
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-11-25 11:54:31 +0800

    add more a flag to check if due to timeout

--- apps/api-gateway/src/lib/api/response-error.ts
+++ apps/api-gateway/src/lib/api/response-error.ts
@@ -1,5 +1,6 @@
 import type { AxiosError } from 'axios';
 
 export interface ResponseError extends AxiosError {
-    isNetworkError: boolean;
+    isNetworkError: boolean;
+    timeout: boolean;// add timeout flag to check if timeout error
 }
