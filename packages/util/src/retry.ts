a9fb9d10be31ab81c7adc86031d6ae1284f2a844
Yoga2015 <yoga3333@126.com>
2023-11-27 21:28:06 +0800

    Use the parameters to specify the name of the retry, the timeout, and the interval between retries.

--- packages/util/src/retry.ts
+++ packages/util/src/retry.ts
@@ -42,6 +42,24 @@ export class Timer {
   }
 }
 
+export function str2Date(dateStr, separator) {
+  if (!separator) {
+    separator = "-";
+  }
+  let dateArr = dateStr.split(separator);
+  let year = parseInt(dateArr[0]);
+  let month;
+  //处理月份为04这样的情况
+  if (dateArr[1].indexOf("0") == 0) {
+    month = parseInt(dateArr[1].substring(1));
+  } else {
+    month = parseInt(dateArr[1]);
+  }
+  let day = parseInt(dateArr[2]);
+  let date = new Date(year, month - 1, day);
+  return date;
+}
+
 /**
  * Retry an asynchronous function until it returns a truthy value or the specified timeout is exceeded.
  * The function is retried periodically with a fixed interval between attempts. The operation can be named for better error messages.
