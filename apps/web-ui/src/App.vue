7970f93844f96f90d580aef4597df1c9a3d2574a
Yoga2015 <yoga3333@126.com>
2023-12-05 20:14:30 +0800

    Adjusting the storage path of routing components

--- apps/web-ui/src/App.vue
+++ apps/web-ui/src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 // @ 这个配置项 只做 联想提示
 import HeaderNavigation from '@/components/headerNavigation.vue'
 import Footer from '@/components/footer.vue';
-import Banner from '@/components/banner.vue';
+import Home from '@/views/Home.vue';
 </script>
 
 <template>
@@ -18,7 +18,7 @@ import Banner from '@/components/banner.vue';
     <el-row>
       <el-col :span="24">
         <div class="grid-content ep-bg-purple-dark" />
-        <Banner />
+        <Home />
       </el-col>
     </el-row>
 
@@ -34,6 +34,4 @@ import Banner from '@/components/banner.vue';
   <RouterView />
 </template>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
---
+++ apps/web-ui/src/views/Home.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Banner from '@/components/banner.vue';
+</script>
+
+<template>
+  <el-col>
+
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content ep-bg-purple-dark" />
+        <Banner />
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content ep-bg-purple-dark" />
+        <Banner />
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content ep-bg-purple-dark" />
+        <Banner />
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content ep-bg-purple-dark" />
+        <Banner />
+      </el-col>
+    </el-row>
+
+  </el-col>
+</template>
