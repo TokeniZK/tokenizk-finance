6a5688466ef0013246e5494b38f761045732da38
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-10 21:05:54 +0800

    define UserState struct

--- packages/contracts/src/TokeniZkUser.ts
+++ packages/contracts/src/TokeniZkUser.ts
@@ -12,3 +12,10 @@ import {
 } from 'o1js';
 import { greaterThanFor254BitField, checkMembershipAndAssert } from '../utils/utils';
 import { UserLowLeafWitnessData, UserNullifierMerkleWitness, LeafData } from './merkle_witness';
+
+export const DUMMY_FIELD = Field(0);
+
+export class UserState extends Struct({
+    nullifierRoot: Field,
+    nullStartIndex: Field,
+}) { }
