147d26e120d2108423b7a25fc8472778b3eef150
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-10 21:05:14 +0800

    import utils and o1js

--- packages/contracts/src/TokeniZkUser.ts
+++ packages/contracts/src/TokeniZkUser.ts
@@ -1,1 +1,14 @@
 
+import {
+    State,
+    state,
+    SmartContract,
+    AccountUpdate,
+    method,
+    Field,
+    Struct,
+    Bool,
+    Provable,
+} from 'o1js';
+import { greaterThanFor254BitField, checkMembershipAndAssert } from '../utils/utils';
+import { UserLowLeafWitnessData, UserNullifierMerkleWitness, LeafData } from './merkle_witness';
