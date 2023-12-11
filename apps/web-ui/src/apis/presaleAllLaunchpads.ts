f07de861110a702a75e48db0b944e885ed5e6501
Tang <143581472+Tang1903@users.noreply.github.com>
2023-12-11 23:05:06 +0800

    "getAllLaunchpadsAPI" Add comment

--- apps/web-ui/src/apis/presaleAllLaunchpads.ts
+++ apps/web-ui/src/apis/presaleAllLaunchpads.ts
@@ -1,4 +1,9 @@
 import httpInstance from '@/utils/http'
+/**
+ * 获取所有Launchpads的API
+ * 
+ * @returns http请求实例
+ */
 export function getAllLaunchpadsAPI() {
   return httpInstance({
     url: '/home/category/head'
