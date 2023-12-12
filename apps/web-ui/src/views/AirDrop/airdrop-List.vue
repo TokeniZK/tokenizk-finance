ea22f98957a778f13f0aa450692d08750220997d
Tang <143581472+Tang1903@users.noreply.github.com>
2023-12-12 22:51:03 +0800

    Configure routing for the "airdrop-List" component

--- apps/web-ui/src/views/AirDrop/airdrop-List.vue
+++ apps/web-ui/src/views/AirDrop/airdrop-List.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    
-  </div>
+  <el-row class="row-bg airdrop-list" justify="center">
+    <el-col :span="24">
+     我是 airdrop-List 组件
+    </el-col>
+  </el-row>
 </template>
+
+<style lang="less" scoped>
+.airdrop-list {
+  width: 100%;
+}
+</style>
