de348b4932fd212c957a139f321eef948168307d
Yoga2015 <yoga3333@126.com>
2023-12-12 22:03:46 +0800

    Optimize the UI of the "mainFunctionIntroduction" component

--- apps/web-ui/src/views/Home/components/mainFunctionIntroduction.vue
+++ apps/web-ui/src/views/Home/components/mainFunctionIntroduction.vue
@@ -1,42 +1,95 @@
 <template>
   <el-row class="row-bg mainFunctionIntroduction" justify="center">
 
-    <el-col :span="4"></el-col>
+    <el-col :span="1"></el-col>
 
     <el-col :span="18">
 
-      <el-row class="row-bg" justify="center">
-        <el-col :span="6">1</el-col>
-        <el-col :span="6">2</el-col>
-        <el-col :span="6">3</el-col>
+      <el-row class="row-bg" justify="space-between">
+        <el-col :span="6" class="category-box">
+          <el-row justify="center"><img src="/src/assets/Customization.png" alt=""
+              style="background-color: #fff;"></el-row>
+          <el-row justify="center">
+            <h4 class="lasthead">Customization </h4>
+          </el-row>
+          <el-row class="category-text">Easily customize your own tokens to create token sales</el-row>
+        </el-col>
+        <el-col :span="6" class="category-box">
+          <el-row justify="center"> <img src="/src/assets/Launchpad.png" alt="" style="background-color: #fff;"></el-row>
+          <el-row justify="center">
+            <h4 class="lasthead">Launchpad</h4>
+          </el-row>
+          <el-row class="category-text">With just a few clicks, you can create a launchpad using the token you
+            cast</el-row>
+        </el-col>
+        <el-col :span="6" class="category-box">
+          <el-row justify="center"> <img src="/src/assets/Present-Presale.png" alt=""></el-row>
+          <el-row justify="center">
+            <h4 class="lasthead">Present Presale</h4>
+          </el-row>
+          <el-row class="category-text">Intuitive display,easy selection of favorite Presales</el-row>
+        </el-col>
       </el-row>
 
-      <el-row class="row-bg" justify="center">
-        <el-col :span="6">4</el-col>
-        <el-col :span="6">5</el-col>
-        <el-col :span="6">6</el-col>
+      <el-row class="row-bg" justify="space-between">
+        <el-col :span="6" class="category-box">
+          <el-row justify="center"><img src="/src/assets/AirdropAndLocker.png" alt=""></el-row>
+          <el-row justify="center">
+            <h4 class="lasthead">Airdrop and Locker</h4>
+          </el-row>
+          <el-row class="category-text">Easily customize your own tokens to create token sales</el-row>
+        </el-col>
+        <el-col :span="6" class="category-box">
+          <el-row justify="center"> <img src="/src/assets/ManagementManager.png" alt=""></el-row>
+          <el-row justify="center">
+            <h4 class="lasthead">Management Manager</h4>
+          </el-row>
+          <el-row class="category-text">This portal helps you easily update the content of launchpad.</el-row>
+        </el-col>
+        <el-col :span="6" class="category-box">
+          <el-row justify="center"> <img src="/src/assets/BrandingBrandStrategy.png" alt=""></el-row>
+          <el-row justify="center">
+            <h4 class="lasthead">Branding brand strategy</h4>
+          </el-row>
+          <el-row class="category-text">Add logos, social links, descriptions, and lists on TokeniZK</el-row>
+        </el-col>
       </el-row>
 
     </el-col>
 
-    <!-- <el-col :span="2"></el-col> -->
+    <el-col :span="1"></el-col>
 
   </el-row>
 </template>
 
 <style lang="less" scoped>
 .mainFunctionIntroduction {
+  margin-bottom: 150px;
   width: 100%;
   padding-top: 200px;
-  height: 600px;
+  height: 700px;
   background-color: #f7f7f7;
+
+  .category-box {
+    margin-bottom: 50px;
+    padding: 30px 20px;
+    width: 380px;
+    height: 200px;
+    text-align: center;
+    background-color: #fff;
+    // border-radius: 180px;
+
+    .category-text {
+      font-size: 13px;
+    }
+  }
 
   .el-row {
     margin-bottom: 20px;
   }
 
   .el-col {
-    border-radius: 4px;
+    border-radius: 20px;
   }
 
   .grid-content {
