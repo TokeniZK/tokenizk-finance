59060af64eba888b6923a18659be05cb70d55f58
Yoga2015 <yoga3333@126.com>
2023-12-15 16:03:50 +0800

    Adjusting the storage location of components

--- apps/web-ui/src/App.vue
+++ apps/web-ui/src/App.vue
@@ -1,26 +1,16 @@
 <script setup lang="ts">
-import HeaderNavigation from '@/components/headerNavigation.vue'
-import Foot from '@/components/footer.vue';
 </script>
 
 <template>
   <el-row>
     <el-col :span="24">
 
-      <!-- <el-row>
-        <HeaderNavigation />
-      </el-row> -->
-
       <el-row>
-        
+
         <!-- 一级路由出口组件 -->
         <RouterView />
 
       </el-row>
-
-      <!-- <el-row>
-        <Foot />
-      </el-row> -->
 
     </el-col>
   </el-row>
