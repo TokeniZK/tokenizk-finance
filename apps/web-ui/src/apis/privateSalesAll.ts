15abb1195dd77d29897e152a948beb960d9f4efc
Yoga2015 <yoga3333@126.com>
2023-12-19 22:31:18 +0800

    Add  "getAllPrivateSales" interface

---
+++ apps/web-ui/src/apis/privateSalesAll.ts
@@ -0,0 +1,11 @@
+import httpInstance from '@/utils/http'
+/**
+ * 获取所有Launchpads的API
+ * 
+ * @returns http请求实例
+ */
+export function getAllPrivateSalesAPI() {
+  return httpInstance({
+    url: '/home/category/head'
+  })
+}
