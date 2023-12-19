aef3e99b175fc2fa171abdc409b209362783ee13
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-19 23:50:55 +0800

    gen SaleReqSchema for fastify endpoint

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,8 +1,18 @@
+import MerkleProofDtoSchema from './merkle-proof-dto-schema.json' assert { type: "json" };
+import SaleReqSchema from './sale-req-schema.json' assert { type: "json" };
 
-import ProofTaskDtoSchma from './proof-task-dto-schema.json' assert { type: "json" };
+export {MerkleProofDtoSchema, SaleReqSchema}
 
-export { ProofTaskDtoSchma };
 
 export * from './proof-task-dto'
 
 export * from './base-response'
+
+export * from './merkle_tree'
+
+
+export * from './merkle-proof-dto'
+
+export * from './constant'
+
+export * from './sale-req'
