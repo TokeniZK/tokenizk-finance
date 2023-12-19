7c208a17b036db1728d83cc28133a036b86b6947
Tang <143581472+Tang1903@users.noreply.github.com>
2023-12-19 16:05:08 +0800

    Auro wallet connection status management

--- apps/web-ui/src/stores/connectState.ts
+++ apps/web-ui/src/stores/connectState.ts
@@ -4,9 +4,8 @@ import { defineStore } from 'pinia'
 export const useConnectState = defineStore('connectState', () => {
 
   // Auro Wallet 连接状态 管理
-  let cnState = false
-
-  ref(cnState)
+  let cnState = ref(cnState)
+  
 
   function setConnectState() {
     return cnState
