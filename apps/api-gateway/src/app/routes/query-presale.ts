43b3aacf28337571fad02b05f0cbef4b092b785c
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-20 23:22:04 +0800

    init endpoint for queryPresale

--- apps/api-gateway/src/app/routes/query-presale.ts
+++ apps/api-gateway/src/app/routes/query-presale.ts
@@ -1,1 +1,61 @@
+import httpCodes from "@inip/http-codes"
+import { FastifyPlugin } from "fastify"
+import { BaseResponse, SaleReq, SaleReqSchema, PresaleDto, PresaleDtoSchema } from '@tokenizk/types'
+import { RequestHandler } from '@/lib/types'
 
+import { getConnection, In } from "typeorm"
+import { getLogger } from "@/lib/logUtils"
+import { Presale, UserTokenPresale } from "@tokenizk/entities"
+
+const logger = getLogger('queryPresaleList');
+
+// query all, filter by status==on, order by createTime, limit 18
+// by tokenName
+// by userAddress 
+export const queryPresaleList: FastifyPlugin = async function (
+    instance,
+    options,
+    done
+): Promise<void> {
+    instance.route({
+        method: "POST",
+        url: "/presale/list",
+        //preHandler: [instance.authGuard],
+        schema,
+        handler
+    })
+}
+
+export const handler: RequestHandler<SaleReq, null> = async function (
+    req,
+    res
+): Promise<BaseResponse<PresaleDto[]>> {
+    const saleReq = req.body
+
+}
+
+const schema = {
+    description: 'query presale list',
+    tags: ["Presale"],
+    body: {
+        type: SaleReqSchema.type,
+        properties: SaleReqSchema.properties
+    },
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                code: {
+                    type: 'number',
+                },
+                data: {
+                    type: PresaleDtoSchema.type,
+                    properties: PresaleDtoSchema.properties
+                },
+                msg: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
