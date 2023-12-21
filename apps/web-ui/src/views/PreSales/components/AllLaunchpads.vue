2be25148feaa9a31ba2bb4f73d6ba730b084040c
Yoga2015 <yoga3333@126.com>
2023-12-21 21:41:57 +0800

    Filter out the target project based on the selected "filter by options"

--- apps/web-ui/src/views/PreSales/components/AllLaunchpads.vue
+++ apps/web-ui/src/views/PreSales/components/AllLaunchpads.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { getAllLaunchpadsAPI } from '@/apis/presaleAll'
 import { onMounted, reactive } from 'vue'
 import { Search } from '@element-plus/icons-vue'
@@ -10,6 +10,7 @@ import { nanoid } from 'nanoid'
 import { Minus, Plus } from '@element-plus/icons-vue'
 // 搜索获取项目
 import { getSearchProjectAPI } from '@/apis/getSearchProjectsApi'
+import { el } from 'element-plus/es/locale'
 
 const percentage = ref(20)
 const customColor = ref('#00FFC2')
@@ -27,83 +28,8 @@ onMounted(() => {
   getAllLaunchpads()
 })
 
-
-// 搜索
-let keyWord = ref('')
-
-const getSearchProjects = async () => {
-  let searchRes = getSearchProjectAPI(keyWord);
-  console.log(searchRes);
-  // allLaunchpadsList.value = searchRes.value
-}
-
-// 过滤器
-const filterBy = ref('')
-const sortBy = ref('')
-
-// 渲染所需的数据
-const filterByOptions = [
-  {
-    value: '0',
-    label: 'All Status',
-  },
-  {
-    value: '1',
-    label: 'Upcoming',
-  },
-  {
-    value: '2',
-    label: 'Ongoing',
-  },
-  {
-    value: '3',
-    label: 'Filled',
-  },
-  {
-    value: '4',
-    label: 'Ended',
-  },
-  // {
-  //   value: '5',
-  //   label: 'Canceled',
-  // },
-]
-
-const sortByOptions = [
-  {
-    value: '0',
-    label: 'No Sort',
-  },
-  {
-    value: '1',
-    label: 'Soft Cap',
-  },
-  {
-    value: '2',
-    label: 'Hard Cap',
-  },
-  // {
-  //   value: '3',
-  //   label: 'LP percent',
-  // },
-  {
-    value: '3',
-    label: 'Start time',
-  },
-  {
-    value: '4',
-    label: 'End time',
-  },
-]
-
-const searchProjects = () => {
-  filterBy.value = '0'
-  sortBy.value = '0'
-}
-
-
-// 临时数据渲染
-let obj = reactive([
+// 切换路由地址时，请求响应回来的数据  渲染出每个项目
+const fetchResult = [
   {
     id: nanoid(),
     photo: '/src/assets/images/1.png',
@@ -112,8 +38,8 @@ let obj = reactive([
     teamName: 'Yoga',
     star: '4',
     preSaleAddr: 'B62',
-    softCap: '20',
-    hardCap: '50',
+    softCap: '21',
+    hardCap: '60',
     totalContributedMina: '40',
     progressStart: '0',
     progressEnd: '50',
@@ -129,7 +55,7 @@ let obj = reactive([
     photo: '/src/assets/images/2.png',
     name: 'Wojak 2.69',
     status: '',
-    teamName: 'Yoga',
+    teamName: 'walking',
     star: '2',
     preSaleAddr: 'B62',
     softCap: '10',
@@ -149,7 +75,7 @@ let obj = reactive([
     photo: '/src/assets/images/3.png',
     name: 'Ripple Frog',
     status: '',
-    teamName: 'Yoga',
+    teamName: 'cherry',
     star: '2',
     preSaleAddr: 'B62',
     softCap: '30',
@@ -166,14 +92,14 @@ let obj = reactive([
   },
   {
     id: nanoid(),
-    photo: '/src/assets/images/1.png',
+    photo: '/src/assets/images/3.png',
     name: 'FastAI',
     status: '',
-    teamName: 'Yoga',
+    teamName: 'Tang',
     star: '4',
     preSaleAddr: 'B62',
     softCap: '10',
-    hardCap: '50',
+    hardCap: '55',
     totalContributedMina: '30',
     progressStart: '10',
     progressEnd: '50',
@@ -189,10 +115,10 @@ let obj = reactive([
     photo: '/src/assets/images/2.png',
     name: 'Wrapped XRP',
     status: '',
-    teamName: 'Yoga',
+    teamName: 'mina',
     star: '2',
     preSaleAddr: 'B62',
-    softCap: '10',
+    softCap: '9',
     hardCap: '50',
     totalContributedMina: '50',
     progressStart: '0',
@@ -206,13 +132,13 @@ let obj = reactive([
   },
   {
     id: nanoid(),
-    photo: '/src/assets/images/3.png',
+    photo: '/src/assets/images/1.png',
     name: 'THREADS V2',
     status: '',
-    teamName: 'Yoga',
+    teamName: 'BTC',
     star: '2',
     preSaleAddr: 'B62',
-    softCap: '10',
+    softCap: '12',
     hardCap: '50',
     totalContributedMina: '10',
     progressStart: '0',
@@ -226,13 +152,13 @@ let obj = reactive([
   },
   {
     id: nanoid(),
-    photo: '/src/assets/images/1.png',
+    photo: '/src/assets/images/2.png',
     name: 'Oggy Inu 2.0',
     status: '',
     teamName: 'Yoga',
     star: '5',
     preSaleAddr: 'B62',
-    softCap: '10',
+    softCap: '11',
     hardCap: '50',
     totalContributedMina: '13',
     progressStart: '23',
@@ -252,7 +178,7 @@ let obj = reactive([
     teamName: 'Yoga',
     star: '2',
     preSaleAddr: 'B62',
-    softCap: '10',
+    softCap: '20',
     hardCap: '50',
     totalContributedMina: '42',
     progressStart: '0',
@@ -284,28 +210,86 @@ let obj = reactive([
     firstReleaseForProject: '95%',
     vestingForProject: '3% each 1 days',
   },
-])
+];
 
-// 处理映射关系
-// obj.map((item) => {
-//   // console.log(item.status);
-//   if (item.status === '0') {
-//     item.status = 'All Status'
-//   } else if (item.status === '1') {
-//     item.status = 'Upcoming'
-//   } else if (item.status === '2') {
-//     item.status = 'Ongoing'
-//   } else if (item.status === '3') {
-//     item.status = 'Filled'
-//   } else if (item.status === '4') {
-//     item.status = 'Ended'
-//   }
-// })
+
+// 获取 用户输入的关键字 进行搜索
+let keyWord = ref('')
+const getSearchProjects = async () => {
+  let searchRes = getSearchProjectAPI(keyWord);
+  console.log(searchRes);
+  // allLaunchpadsList.value = searchRes.value;
+}
+
+// 过滤器
+const filterBy = ref('')
+const sortBy = ref('')
+
+// fitlerBy 下拉菜单 渲染所需的数据
+const filterByOptions = [
+  {
+    value: '0',
+    label: 'All Status',
+  },
+  {
+    value: '1',
+    label: 'Upcoming',
+  },
+  {
+    value: '2',
+    label: 'Ongoing',
+  },
+  {
+    value: '3',
+    label: 'Filled',
+  },
+  {
+    value: '4',
+    label: 'Ended',
+  },
+  // {
+  //   value: '5',
+  //   label: 'Canceled',
+  // },
+]
+
+// sortBy 下拉菜单 渲染所需的数据
+const sortByOptions = [
+  {
+    value: '0',
+    label: 'No Sort',
+  },
+  {
+    value: '1',
+    label: 'Soft Cap',
+  },
+  {
+    value: '2',
+    label: 'Hard Cap',
+  },
+  {
+    value: '3',
+    label: 'Start time',
+  },
+  {
+    value: '4',
+    label: 'End time',
+  },
+  // {
+  //   value: '5',
+  //   label: 'LP percent',
+  // },
+]
+
+// 每次点击 搜索按钮 时，重置 过滤选项 和 排序选项
+const searchProjects = () => {
+  filterBy.value = '0'
+  sortBy.value = '0'
+}
 
 // 判断项目的状态
 let currentTime = new Date().getTime();
-
-obj.filter(item => {
+fetchResult.forEach(item => {
   if (item.presaleStartTime > currentTime) {
     item.status = 'Upcoming'
   } else if (item.presaleStartTime <= currentTime && item.presaleEndTime > currentTime) {
@@ -315,25 +299,120 @@ obj.filter(item => {
   } else {
     item.status = 'All Status'
   }
-})
+});
 
+// sort   相等运算符会转类型、 最后一项没使用Number做转换
+if (sortBy.value == '1') {
+  fetchResult.sort((a, b) => {
+    return Number(a.softCap) - Number(b.softCap);
+  });
+} else if (sortBy.value == '2') {
+  fetchResult.sort((a, b) => {
+    return Number(b.hardCap) - Number(a.hardCap);
+  });
+} else if (sortBy.value == '3') {
+  fetchResult.sort((a, b) => {
+    return Number(b.presaleStartTime) - Number(a.presaleStartTime);
+  });
+} else if (sortBy.value == '4') {
+  fetchResult.sort((a, b) => {
+    return b.presaleEndTime - a.presaleEndTime;
+  });
+}
+
+let renderSaleBlock = fetchResult;
+// 临时数据
+let obj = reactive({ saleList: renderSaleBlock });
+
+// 根据 用户选择 filterBy的选项  过滤数据
 const filterOption = (option: string) => {
 
-  const filterProjects = obj.filter(item => { 
-    if (item.status === option) {
-      return item
+  // console.log(option);
+
+  currentTime = new Date().getTime();
+  fetchResult.forEach(item => {
+    if (item.presaleStartTime > currentTime) {
+      item.status = 'Upcoming'
+    } else if (item.presaleStartTime <= currentTime && item.presaleEndTime > currentTime) {
+      item.status = 'Ongoing'
+    } else if (item.presaleEndTime < currentTime) {
+      item.status = 'Ended'
+    } else {
+      item.status = 'All Status'
     }
-  })
+  });
+
+
+  if (option === '3') {
+    // TODO 
+    fetchResult.filter(item => {
+      return item.status === 'All Status'
+    })
+
+  } else if (option === '0') {
+    renderSaleBlock = fetchResult
+  } else {
+    renderSaleBlock = fetchResult.filter(item => {
+      if (option == '1') {
+        return item.status === 'Upcoming'
+      } else if (option == '2') {
+        return item.status === 'Ongoing'
+      } else if (option == '3') {
+        return item.status === 'Ended'
+      }
+    });
+
+    // sort
+    if (sortBy.value == '1') {
+      renderSaleBlock.sort((a, b) => {
+        return Number(a.softCap) - Number(b.softCap);
+      });
+    } else if (sortBy.value == '2') {
+      renderSaleBlock.sort((a, b) => {
+        return Number(b.hardCap) - Number(a.hardCap);
+      });
+    } else if (sortBy.value == '3') {
+      renderSaleBlock.sort((a, b) => {
+        return Number(b.presaleStartTime) - Number(a.presaleStartTime);
+      });
+    } else if (sortBy.value == '4') {
+      renderSaleBlock.sort((a, b) => {
+        return Number(b.presaleEndTime) - Number(a.presaleEndTime);
+      });
+    }
+
+    obj.saleList = renderSaleBlock;
+  }
 
 }
 
 
+// 根据 用户选择 sortBy的选项 sort排序 由近到远
+const sortOption = (option: string) => {
 
-// sort排序 由近到远
-// obj.sort()
+  console.log(option);
 
-
-
+  // sort
+  if (sortBy.value == '1') {
+    renderSaleBlock.sort((a, b) => {
+      return Number(a.softCap) - Number(b.softCap);
+    });
+  } else if (sortBy.value == '2') {
+    renderSaleBlock.sort((a, b) => {
+      return Number(b.hardCap) - Number(a.hardCap);
+    });
+  } else if (sortBy.value == '3') {
+    renderSaleBlock.sort((a, b) => {
+      return Number(b.presaleStartTime) - Number(a.presaleStartTime);
+    });
+  } else if (sortBy.value == '4') {
+    renderSaleBlock.sort((a, b) => {
+      return b.presaleEndTime - a.presaleEndTime;
+    });
+  }
+  renderSaleBlock = JSON.parse(JSON.stringify(renderSaleBlock))
+  obj.saleList = renderSaleBlock;
+}
 
 
 </script>
@@ -371,7 +450,8 @@ const filterOption = (option: string) => {
         <el-col :span="3">
           <div>Sort By</div>
           <el-select v-model="sortBy" class="m-2 sortBy" placeholder="Select" size="large">
-            <el-option v-for="item in sortByOptions" :key="item.value" :label="item.label" :value="item.value" />
+            <el-option v-for="item in sortByOptions" :key="item.value" :label="item.label" :value="item.value"
+              @click="sortOption(item.value)" />
           </el-select>
         </el-col>
 
@@ -382,37 +462,13 @@ const filterOption = (option: string) => {
         <el-col :span="20">
 
           <ul class="launchpads-ul">
-            <li v-for="item in obj" :key="item.id">
+            <li v-for="item in obj.saleList" :key="item.id">
 
               <div class="launchpads-box">
 
                 <!-- photo -->
                 <el-row class="thumb">
-
-                  <el-col>
-
-                    <el-row>
-                      <img :src="item.photo" :alt="item.name" style="width: 349px; height: 160px;">
-                    </el-row>
-
-                    <el-row class="button-box">
-                      <el-col>
-                        <a href="Ripple-Frog-presale-details.html" class="link link-1">
-                          <el-icon>
-                            <View />
-                          </el-icon>
-                        </a>
-                      </el-col>
-                      <el-col>
-                        <a href="#0" class="link link-3"><el-icon>
-                            <StarFilled />
-                          </el-icon>
-                        </a>
-                      </el-col>
-                    </el-row>
-
-                  </el-col>
-
+                  <el-image :src="item.photo" :alt="item.name" loading="lazy" :fit="contain" />
                 </el-row>
 
                 <!-- 项目描述 -->
@@ -428,12 +484,12 @@ const filterOption = (option: string) => {
                       <el-col :span="5"></el-col>
 
                       <el-col class="review" :span="7">
-                        <el-button type="primary" round>{{ item.status }}</el-button>
+                        <el-button type="primary" round class="statusColor">{{ item.status }}</el-button>
                       </el-col>
                     </el-row>
 
                     <!-- 团队名称 -->
-                    <el-row class="text-review-change" justify="space-between" style="align-items: center;">
+                    <el-row class="row-bg" justify="space-between" style="align-items: center;">
                       <el-col class="text" :span="10">
                         by <a href="" class="link">{{ item.teamName }}</a>
                       </el-col>
@@ -525,7 +581,6 @@ const filterOption = (option: string) => {
         width: 100%;
         height: 100%;
         background-color: #fff;
-        // border: 1px solid yellow;
         border-radius: 15px;
 
         .thumb {
