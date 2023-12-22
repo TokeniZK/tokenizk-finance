43075550ae0ed0c5fed88c602bbcdc0bde335286
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-22 23:41:09 +0800

    filterStep and reducerStep for presale

--- apps/proof-gen/src/provers/presale-rollup-handler.ts
+++ apps/proof-gen/src/provers/presale-rollup-handler.ts
@@ -9,4 +9,29 @@ import { getDateString } from '@/lib';
 const logger = getLogger('presale-rollup-handler');
 
 export const presaleRollupBatchAndMerge = async (subProcessCordinator: SubProcessCordinator, proofPayloads: ProofPayload<any>[], sendCallBack?: any) => {
+const filterStep = (openTasks: ProofPayload<any>[]) => {
+        return openTasks;
+    };
+    const reducerStep = async (proofPayLoadList: ProofPayload<any>[]) => {
+        if (proofPayLoadList.length == 1) return [];
+
+        let promises: Promise<any>[] = [];
+        if (!proofPayLoadList[0].isProof) {
+            for (let i = 0; i < proofPayLoadList.length; i++) {
+                promises.push(subProcessCordinator.presaleContributorsBatch(proofPayLoadList[i]));
+            }
+        } else {
+            for (let i = 0; i < proofPayLoadList.length; i = i + 2) {
+                if (proofPayLoadList[i + 1]) {
+                    promises.push(subProcessCordinator.presaleContributorsMerge(proofPayLoadList[i], proofPayLoadList[i + 1]));
+                } else {
+                    promises.push(Promise.resolve(proofPayLoadList[i]));
+                }
+            }
+        }
+
+        proofPayLoadList = await Promise.all(promises);
+        return proofPayLoadList;
+    };
+
 }
