d48d5f8e7869f1e2a5de104b8c6a00ce1c0963b1
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-23 23:08:52 +0800

    import dependency

--- packages/contracts/src/TokeniZkFactory.ts
+++ packages/contracts/src/TokeniZkFactory.ts
@@ -1,1 +1,23 @@
+import {
+    State,
+    state,
+    UInt64,
+    SmartContract,
+    PublicKey,
+    Permissions,
+    Field,
+    Reducer,
+    method,
+    Struct,
+    Poseidon,
+    VerificationKey,
+    AccountUpdate,
+    Experimental,
+    Bool,
+    Provable,
+} from 'o1js';
+import { PrivateSaleParams } from './privatesale/TokeniZkPrivateSale';
+import { INDEX_TREE_INIT_ROOT_8, STANDARD_TREE_INIT_ROOT_16 } from './constants';
+import { RedeemAccount } from './user';
+
 
