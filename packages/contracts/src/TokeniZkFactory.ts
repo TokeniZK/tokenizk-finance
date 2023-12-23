add4a317ee65a1b320c7468a680ef561d2d5505f
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-23 23:09:27 +0800

    define LauchpadPlatformParams

--- packages/contracts/src/TokeniZkFactory.ts
+++ packages/contracts/src/TokeniZkFactory.ts
@@ -21,3 +21,47 @@ import { INDEX_TREE_INIT_ROOT_8, STANDARD_TREE_INIT_ROOT_16 } from './constants'
 import { RedeemAccount } from './user';
 
 
+export class LauchpadPlatformParams extends Struct({
+    basicTokenVk: Field,
+    basicTokenCreationFee: UInt64,
+
+    presaleContractVk: Field,
+    presaleCreationFee: UInt64,
+    presaleServiceFeeRate: UInt64,
+    presaleMinaFundHolderVk: Field,
+
+    fairSaleContractVk: Field,
+    fairSaleCreationFee: UInt64,
+    fairSaleServiceFeeRate: UInt64,
+
+    privateSaleContractVk: Field,
+    privateSaleCreationFee: UInt64,
+    privateSaleServiceFeeRate: UInt64,
+    privateSaleMinaFundHolderVk: Field,
+
+    redeemAccountVk: Field
+}) {
+
+    hash() {
+        return Poseidon.hash([
+            this.basicTokenVk,
+            ...this.basicTokenCreationFee.toFields(),
+
+            this.presaleContractVk,
+            ...this.presaleCreationFee.toFields(),
+            ...this.presaleServiceFeeRate.toFields(),
+
+            this.fairSaleContractVk,
+            ... this.fairSaleCreationFee.toFields(),
+            ... this.fairSaleServiceFeeRate.toFields(),
+
+            this.presaleContractVk,
+            ... this.privateSaleCreationFee.toFields(),
+            ...this.privateSaleServiceFeeRate.toFields(),
+
+            this.redeemAccountVk
+        ]);
+    }
+
+}
+
