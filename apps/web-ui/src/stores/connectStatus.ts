3c7da18bc32767889d011e3c7ed965cffdee5357
Yoga2015 <yoga3333@126.com>
2023-12-23 14:55:01 +0800

    Set functions based on project pre-sale time

--- apps/web-ui/src/stores/connectStatus.ts
+++ apps/web-ui/src/stores/connectStatus.ts
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const useConnectStatusStore = defineStore('connectStatus', () => {
 
   // Auro Wallet 连接状态 管理
-  let cnState = ref(false)
+  let cnState = ref(true)
 
   function setConnectStatus() {
     return cnState
--- apps/web-ui/src/views/PreSales/components/presale-details.vue
+++ apps/web-ui/src/views/PreSales/components/presale-details.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ref, onMounted, reactive } from 'vue'
-import { Minus, Plus } from '@element-plus/icons-vue'
+import { Minus, Plus, Wallet } from '@element-plus/icons-vue'
 import dayjs from 'dayjs'
 import { Calendar } from '@element-plus/icons-vue'
 import { useConnectStatusStore } from '@/stores/connectStatus'
@@ -17,8 +17,8 @@ onMounted(() => {
 
 })
 
-// 切换路由地址时，请求响应回来的数据  渲染出项目
-const fetchResult = reactive({
+// 临时数据本尊
+const originalResult = {
   id: nanoid(),
   photo: '/src/assets/images/1.png',
   name: 'Oggy Inu 2.0',
@@ -26,59 +26,44 @@ const fetchResult = reactive({
   star: '4',
   preSaleAddr: 'B62',
   softCap: 300,
-  hardCap: 60,
+  hardCap: 600,
   totalContributedMina: 40,
   progressStart: 0,
   progressEnd: 50,
   liquidity: '10%',
   lockupTime: '365day',
-  presaleStartTime: 1701083125572,
-  presaleEndTime: 1705093115572,
+  presaleStartTime: 1702083125572,
+  presaleEndTime: 1703312210497,
   firstReleaseForProject: '95%',
   vestingForProject: '3% each 1 days',
   projectDes: 'TokeniZK Finance is a decentralized launchpad where you could launch your own zk-Token and create your own initial token sale. It provides secure smart contract templates with flexible configuration and complete tool suits for token management, where you could finish all operations simply in several clicks, without knowledge requirement about code & zkp. ',
   rate: 2,
   contributedMinaAmount: 350,
-  recievedTokenAmount: 600,
+  recievedTokenAmount: 700,
   saleType: 'public',
   minimumBuy: 0.3,
   maximumBuy: 1,
-})
+  contractBalance: 300
+};
 
+// 临时数据本尊的分身   用于渲染出项目
+const fetchResult = reactive(originalResult)
 
-localStorage.setItem('presaleProject', JSON.stringify(fetchResult))
+// localStorage.setItem('presaleProject', JSON.stringify(fetchResult))
 
 
 // 进度条
 const percentage = ref(20)
 const customColor = ref('#00FFC2')
 
-let currentTime = new Date().getTime();
-
 // 倒计时
-const timeValue = ref(dayjs().add(1, 'month').startOf('month'))
+let currentTime = new Date().getTime();
+const timeValue = ref(dayjs().add(originalResult.presaleEndTime - currentTime, 'millisecond'))
 
 const tokenInput = ref('')
 
-
-
-// 钱包连接状态判断
-let connectStatus = useConnectStatusStore();
-let { cnState } = connectStatus;
-
-// 判断项目预售时间
-const projectStatus = ref('Buy with Mina')
-
-if (cnState === false) {
-  projectStatus.value = 'Connect Auro Wallet'
-} else if (cnState === true && fetchResult.contributedMinaAmount <= 0) {
-  projectStatus.value = 'Buy with Mina'
-} else if (cnState === true && fetchResult.contributedMinaAmount > 0 && fetchResult.presaleEndTime > currentTime) {
-  projectStatus.value = 'claim'
-} else if (cnState === true && fetchResult.contributedMinaAmount > 0 && fetchResult.presaleEndTime < currentTime) {
-  projectStatus.value = 'redeem'
-}
-
+const contributionInputDisabled = ref(false);
+const contributionBtnDisabled = ref(false);
 
 
 if (fetchResult.presaleStartTime > currentTime) {
@@ -89,7 +74,103 @@ if (fetchResult.presaleStartTime > currentTime) {
   fetchResult.status = 'Ended'
 }
 
+// 判断项目预售时间
+const connectAuroWallet = ref('Connect Auro Wallet')
+const buyWithMina = ref('Buy with Mina')
+const claimTokens = ref('claim tokens')
+const redeemTokens = ref('redeem tokens')
+const detailSpan = ref('已投资，不可重复投资.')
 
+const flagSpan = ref(0)
+
+// 钱包连接状态判断
+let connectStatus = useConnectStatusStore()
+let { cnState } = connectStatus
+
+let flagBtn = ref(0)
+
+const checkPresale = () => {
+
+  if (fetchResult.presaleEndTime < currentTime) {
+
+    // presaleEndTime 已结束
+    if (cnState === true) {
+      // 已投资
+      if (fetchResult.contributedMinaAmount > 0) {
+
+        // 总投资 >= softCap
+        if (fetchResult.contributedMinaAmount >= fetchResult.softCap) {
+          flagBtn.value = 2
+          contributionInputDisabled.value = true   // 禁用input
+          contributionBtnDisabled.value = false   //  启用按钮
+          tokenInput.value = fetchResult.recievedTokenAmount
+        } else {
+          flagBtn.value = 3
+          contributionInputDisabled.value = true   // 禁用input
+          contributionBtnDisabled.value = false   //  启用按钮
+          tokenInput.value = fetchResult.recievedTokenAmount
+        }
+      } else {
+        // 禁用input
+        contributionInputDisabled.value = true
+        contributionBtnDisabled.value = true
+      }
+
+    } else {
+
+      // 禁用input
+      contributionInputDisabled.value = true
+      contributionBtnDisabled.value = true
+
+    }
+
+  } else {
+
+    // presaleEndTime 没结束
+    if (cnState === true) {
+
+      if (fetchResult.contributedMinaAmount > 0) {
+        // 禁用input、按钮
+        contributionInputDisabled.value = true
+        contributionBtnDisabled.value = true
+        flagBtn.value = 1
+        flagSpan.value = 1
+      }
+
+    } else {
+
+      // 启用input、按钮
+      contributionInputDisabled.value = false
+      contributionBtnDisabled.value = false
+      flagBtn.value = 1
+
+    }
+
+
+  }
+
+}
+
+
+checkPresale()
+
+
+const checkContribution = () => {
+  checkPresale()
+}
+
+
+
+const countdownFinishCallback = () => {
+  // 禁用input
+  contributionInputDisabled.value = true;
+  contributionBtnDisabled.value = true;
+
+  // 修改status 为 Ended
+  fetchResult.status = 'Ended'
+
+  checkContribution();
+}
 
 </script>
 
@@ -137,12 +218,12 @@ if (fetchResult.presaleStartTime > currentTime) {
                 <el-col :span="12"> {{ fetchResult.hardCap }}</el-col>
               </el-row>
               <el-row class="row-bg" justify="space-between">
-                <el-col :span="12">Presale Start Time</el-col>
-                <el-col :span="12">{{ new Date(fetchResult.presaleStartTime) }}</el-col>
+                <el-col :span="11">Presale Start Time</el-col>
+                <el-col :span="13">{{ new Date(fetchResult.presaleStartTime) }}</el-col>
               </el-row>
               <el-row class="row-bg" justify="space-between">
-                <el-col :span="12">Presale End Time</el-col>
-                <el-col :span="12">{{ new Date(fetchResult.presaleEndTime) }}</el-col>
+                <el-col :span="11">Presale End Time</el-col>
+                <el-col :span="13">{{ new Date(fetchResult.presaleEndTime) }}</el-col>
               </el-row>
 
               <el-row class="row-bg" justify="space-between">
@@ -165,46 +246,60 @@ if (fetchResult.presaleStartTime > currentTime) {
     <!-- 项目状态 -->
     <el-col :span="6" class="project-status">
 
-      <el-row class="alert-message">
-        Make sure the website is tokenizk.finance !
-      </el-row>
+      <div class="project-status-box">
+        <el-row class="alert-message">
+          Make sure the website is tokenizk.finance !
+        </el-row>
 
-      <el-row class="countdown">
-        <el-col></el-col>
-        <el-col>
-          <el-countdown format="DD [days] HH:mm:ss" :value="timeValue">
-            <template #title>
-              <div style="display: inline-flex; align-items: center">
-                End of pre-sale
-              </div>
-            </template>
-          </el-countdown>
-        </el-col>
-        <el-col></el-col>
-      </el-row>
+        <el-row class="countdown">
+          <el-col>
 
-      <!-- 进度条 -->
-      <el-row>
-        <el-col>
-          <el-row class="Progress demo-progress" style="margin-bottom: 0;">
-            <el-progress :text-inside="true" :stroke-width="14" :percentage="20" />
-          </el-row>
+            <el-countdown format="DD [days] HH:mm:ss" :value="timeValue" @finish="countdownFinishCallback">
+              <template #title>
+                <div style="display: inline-flex; align-items: center">End of pre-sale</div>
+              </template>
+            </el-countdown>
 
-          <el-row class="row-bg" justify="space-between">
-            <el-col :span="10"> 0 Mina</el-col>
-            <!-- <el-col :span="8"></el-col> -->
-            <el-col :span="6">50 Mina</el-col>
-          </el-row>
-        </el-col>
-      </el-row>
+          </el-col>
+        </el-row>
 
-      <el-row>Amount</el-row>
+        <!-- 进度条 -->
+        <el-row>
+          <el-col>
+            <el-row class="Progress demo-progress" style="margin-bottom: 0;">
+              <el-progress :text-inside="true" :stroke-width="14" :percentage="20" />
+            </el-row>
 
-      <el-row>
-        <el-input v-model="tokenInput" placeholder="Please input" size="large" clearable />
-      </el-row>
+            <el-row class="row-bg" justify="space-between">
+              <el-col :span="10"> 0 Mina</el-col>
+              <!-- <el-col :span="8"></el-col> -->
+              <el-col :span="6">50 Mina</el-col>
+            </el-row>
+          </el-col>
+        </el-row>
 
-      <el-button type="primary">{{ projectStatus }}</el-button>
+        <el-row>Amount</el-row>
+
+        <el-row>
+          <el-input v-model="tokenInput" placeholder="Please input" size="large" clearable
+            :disabled="contributionInputDisabled" />
+        </el-row>
+
+        <el-row>
+          <el-button type="primary" :disabled="contributionBtnDisabled" v-show="flagBtn === 0">{{
+            connectAuroWallet }}</el-button>
+          <el-button type="primary" :disabled="contributionBtnDisabled" v-show="flagBtn === 1">{{ buyWithMina
+          }}</el-button>
+          <el-button type="primary" :disabled="contributionBtnDisabled" v-show="flagBtn === 2">{{ claimTokens
+          }}</el-button>
+          <el-button type="primary" :disabled="contributionBtnDisabled" v-show="flagBtn === 3">{{ redeemTokens
+          }}</el-button>
+
+          <span class="detail-span" v-show="flagSpan === 1">{{ detailSpan }}</span>
+
+        </el-row>
+
+      </div>
 
       <!-- 项目表格 -->
       <el-row class="row-bg formTable">
@@ -252,18 +347,28 @@ if (fetchResult.presaleStartTime > currentTime) {
   }
 
   .project-status {
-    background-color: #fff;
-    padding: 30px;
     margin-left: 30px;
 
-    .countdown {
-      text-align: center;
+    .project-status-box {
+      padding: 30px;
+      background-color: #fff;
+
+      .countdown {
+        text-align: center;
+      }
+
+      .detail-span {
+        color: red;
+        margin-left: 10px;
+      }
     }
+
   }
 
   .formTable {
     margin-top: 20px;
-    padding: 20px;
+    padding: 30px;
+    background-color: #fff;
 
     .el-row {
       border-bottom: 1px solid #e6e6e6;
