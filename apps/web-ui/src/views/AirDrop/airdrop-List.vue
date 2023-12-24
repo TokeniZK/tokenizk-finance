ee2bf5ed501da3465aea4f0e2be22aaa3328950d
Yoga2015 <yoga3333@126.com>
2023-12-24 16:36:15 +0800

    Added Airdrop component

--- apps/web-ui/src/views/AirDrop/airdrop-List.vue
+++ apps/web-ui/src/views/AirDrop/airdrop-List.vue
@@ -1,11 +1,55 @@
-<script lang="ts" >
+<script setup lang="ts">
+import { onMounted } from 'vue'
+// 组件挂载完成后执行的函数
+onMounted(() => {
 
+  // 进入当前组件都会回到顶部
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // 平滑滚动到顶部  
+  });
+
+})
 </script>
-
 <template>
-  <el-row class="row-bg airdrop-list" justify="center">
+  <el-row class="row-bg airdrop-list">
     <el-col :span="24">
-      airdrop-List 组件
+
+      <el-row class="row-bg" justify="center">
+        <el-col :span="18">
+          <h1>AirDrop</h1>
+        </el-col>
+      </el-row>
+
+
+      <el-row style="margin:50px auto;" justify="center">
+
+        <el-col :span="5"></el-col>
+
+        <el-col :span="6">
+          <router-link to="/airdrop-list" class="bline">All Airdrops</router-link>
+        </el-col>
+
+        <el-col :span="6">
+          <router-link to="/airdrop-list/my-airdrop" class="bline">My Airdrops</router-link>
+        </el-col>
+
+        <el-col :span="6">
+          <router-link to="/airdrop-list/created-by-you" class="bline">Created By You</router-link>
+        </el-col>
+
+      </el-row>
+
+
+      <!-- 三级路由出口组件 -->
+      <el-row class="row-bg" justify="center">
+        <el-col :span="24">
+
+          <router-view />
+
+        </el-col>
+      </el-row>
+
     </el-col>
   </el-row>
 </template>
@@ -13,5 +57,27 @@
 <style lang="less" scoped>
 .airdrop-list {
   width: 100%;
+  padding-top: 120px;
+
+  .bline:hover {
+    border-bottom: 2px solid #00FFC2;
+  }
+
+  .el-row {
+    margin-bottom: 20px;
+  }
+
+  .el-row:last-child {
+    margin-bottom: 0;
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 }
 </style>
