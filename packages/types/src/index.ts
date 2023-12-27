e8d90f3dfd868df74173d2ad1a8e0df48d4d0138
cherryweb3 <151454267+cherryweb3@users.noreply.github.com>
2023-12-27 12:39:54 +0800

    export schema dto

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,34 +1,35 @@
 import MerkleProofDtoSchema from './merkle-proof-dto-schema.json' assert { type: "json" };
-import PresaleDtoSchema from './presale-dto-schema.json' assert { type: "json" };
-import FairsaleDtoSchema from './fairsale-dto-schema.json' assert { type: "json" };
-
-import PresaleUserDtoSchema from './presale-user-dto-schema.json' assert { type: "json" };
-import FairsaleUserDtoSchema from './fairsale-user-dto-schema.json' assert { type: "json" };
 
 import SaleReqSchema from './sale-req-schema.json' assert { type: "json" };
+
+import SaleDtoSchema from './sale-dto-schema.json' assert { type: "json" };
+
+import SaleUserDtoSchema from './sale-user-dto-schema.json' assert { type: "json" };
+
 import ProofTaskDtoSchema from "./proof-task-dto-schema.json" assert { type: "json" };
 
-export { MerkleProofDtoSchema, PresaleDtoSchema, FairsaleDtoSchema, SaleReqSchema, PresaleUserDtoSchema, FairsaleUserDtoSchema, ProofTaskDtoSchema }
+import UserRedeemClaimWitnessDtoSchema from "./user-redeem-claim-witness-dto-schema.json" assert { type: "json" };
 
+export { MerkleProofDtoSchema, SaleDtoSchema, SaleReqSchema, SaleUserDtoSchema, ProofTaskDtoSchema, UserRedeemClaimWitnessDtoSchema }
+
+
+export * from './fetch-events-standard-response'
 
 export * from './proof-task-dto'
 
 export * from './base-response'
 
-export * from './merkle_tree'
-
-
 export * from './merkle-proof-dto'
 
 export * from './constant'
 
-export * from './presale-dto'
-export * from './fairsale-dto'
-export * from './fairsale-user-dto'
-export * from './presale-user-dto'
+export * from './sale-dto'
+
+export * from './sale-user-dto'
 
 export * from './sale-req'
 
-export * from './fetch-events-standard-response'
 
 export * from './proof-task-dto'
+
+export * from './user-redeem-claim-witness-dto'
