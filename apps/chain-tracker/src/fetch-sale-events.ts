4d73a81882896f93a7761713798c432e41e40a9c
Tang <143581472+Tang1903@users.noreply.github.com>
2023-12-30 16:52:15 +0800

    Traverse the event list, process configuration parameter events, contribution events, and claim events, and save relevant data to the database

--- apps/chain-tracker/src/fetch-sale-events.ts
+++ apps/chain-tracker/src/fetch-sale-events.ts
@@ -38,7 +38,130 @@ export async function standardFetchSaleEvents() {
         })) ?? [];
 
         
+       for (let i = 0; i < presaleList.length; i++) {
+            const sale = presaleList[i];
 
+            const queryRunner = connection.createQueryRunner();
+            try {
+                await queryRunner.startTransaction();
+
+                let saleEventFetchRecord = await queryRunner.manager.findOne(SaleEventFetchRecord, { saleId: sale.id });
+
+                let startBlock = 0;
+                if (saleEventFetchRecord) {
+                    startBlock = saleEventFetchRecord.blockHeight + 1;
+                } else {
+                    saleEventFetchRecord = new SaleEventFetchRecord();
+                    saleEventFetchRecord.tokenAddress = sale.tokenAddress;
+                    saleEventFetchRecord.saleAddress = sale.saleAddress;
+                    saleEventFetchRecord.blockHeight = 0;
+                }
+
+                let tokenzkSaleContract: TokeniZkPresale | TokeniZkFairSale | TokeniZkPrivateSale;
+                let presaleMinaFundHolderContract: PresaleMinaFundHolder;
+                if (sale.saleType == SaleType.PRESALE) {
+                    tokenzkSaleContract = new TokeniZkPresale(PublicKey.fromBase58(sale.saleAddress), TokenId.derive(PublicKey.fromBase58(sale.tokenAddress)));
+                    presaleMinaFundHolderContract = new PresaleMinaFundHolder(PublicKey.fromBase58(sale.saleAddress));
+                } else if (sale.saleType == SaleType.FAIRSALE) {
+                    tokenzkSaleContract = new TokeniZkFairSale(PublicKey.fromBase58(sale.saleAddress), TokenId.derive(PublicKey.fromBase58(sale.tokenAddress)));
+                } else if (sale.saleType == SaleType.PRIVATESALE) {
+                    tokenzkSaleContract = new TokeniZkPrivateSale(PublicKey.fromBase58(sale.saleAddress), TokenId.derive(PublicKey.fromBase58(sale.tokenAddress)));
+                }
+                // fetch events
+                const eventList: EventsStandardResponse[] = await tokenzkSaleContract!.fetchEvents(new UInt32(startBlock));
+                if (sale.saleType == SaleType.PRESALE) {
+                    eventList.push(...(await presaleMinaFundHolderContract!.fetchEvents(new UInt32(startBlock))) ?? []);
+                }
+                eventList.sort((a, b) => a.blockHeight - b.blockHeight);
+
+                for (let i = 0; i < eventList.length; i++) {
+                    const e = eventList[i];
+                    const blockHeight = Number(e.blockHeight.toBigint());
+
+                    if (e.type == 'configureSaleParams') {
+                        const saleParamsConfigurationEvent: SaleParamsConfigurationEvent = e.event.data;
+                        const saleParams = saleParamsConfigurationEvent.saleParams;
+
+                        sale.tokenAddress = saleParams.tokenAddress.toBase58();
+                        sale.totalSaleSupply = Number(saleParams.totalSaleSupply.toString());
+                        sale.saleRate = Number(saleParams.saleRate.toString());
+                        sale.whitelistTreeRoot = saleParams.whitelistTreeRoot.toString();
+                        sale.softCap = Number(saleParams.softCap.toString());
+                        sale.hardCap = Number(saleParams.hardCap.toString());
+                        sale.minimumBuy = Number(saleParams.hardCap.toString());
+                        sale.maximumBuy = Number(saleParams.hardCap.toString());
+                        sale.startTimestamp = Number(saleParams.startTime.toString());
+                        sale.endTimestamp = Number(saleParams.endTime.toString());
+                        sale.cliffTime = Number(saleParams.cliffTime.toString());
+                        sale.cliffAmountRate = Number(saleParams.cliffAmountRate.toString());
+                        sale.vestingPeriod = Number(saleParams.vestingPeriod.toString());
+                        sale.vestingIncrement = Number(saleParams.vestingIncrement.toString());
+
+                        await queryRunner.manager.save(sale);
+
+                    } else if (e.type == 'contribute') {
+                        const contributionEvent: ContributionEvent = e.event.data;
+
+                        const user = new UserTokenSale();
+                        user.contributeTxHash = e.event.transactionInfo.transactionHash;
+                        user.contributorAddress = contributionEvent.contributorAddress.toBase58();
+                        user.saleId = sale.id;
+                        user.saleAddress = sale.saleAddress;
+                        user.tokenAddress = sale.tokenAddress;
+                        user.tokenId = contributionEvent.tokenId.toString();
+                        user.contributeBlockHeight = blockHeight;
+                        user.contributeCurrencyAmount = contributionEvent.minaAmount.toString();
+
+                        await queryRunner.manager.save(user);
+
+                    } else if (e.type == 'claimToken') {
+                        const claimTokenEvent: ClaimTokenEvent = e.event.data;
+
+                        const user = (await queryRunner.manager.find(UserTokenSale, {
+                            tokenAddress: sale.tokenAddress,
+                            saleAddress: sale.saleAddress,
+                            contributorAddress: claimTokenEvent.presaleContribution.contributorAddress.toBase58()
+                        }))![0];
+                        user.claimTxHash = e.event.transactionInfo.transactionHash;
+                        user.redeemOrClaimBlockHeight = blockHeight;
+                        user.claimAmount = claimTokenEvent.presaleContribution.minaAmount.toString();
+
+                        await queryRunner.manager.save(user);
+
+                        ifNotifySyncNullifier = true;
+
+                    } else if (e.type == 'redeem') {
+                        const redeemTokenEvent: RedeemEvent = e.event.data;
+
+                        const user = (await queryRunner.manager.find(UserTokenSale, {
+                            tokenAddress: sale.tokenAddress,
+                            saleAddress: sale.saleAddress,
+                            contributorAddress: redeemTokenEvent.saleContribution.contributorAddress.toBase58()
+                        }))![0];
+                        user.redeemTxHash = e.event.transactionInfo.transactionHash;
+                        user.redeemOrClaimBlockHeight = blockHeight;
+                        await queryRunner.manager.save(user);
+
+                        ifNotifySyncNullifier = true;
+                    }
+
+                    // save to saleEventFetchRecord
+                    saleEventFetchRecord.blockHeight = blockHeight;
+                    await queryRunner.manager.save(saleEventFetchRecord);
+                }
+
+                if (eventList.length > 0) {
+                    saleEventFetchRecord.blockHeight = Number(eventList[eventList.length - 1].blockHeight.toBigint());
+                }
+                await queryRunner.manager.save(saleEventFetchRecord);
+
+                await queryRunner.commitTransaction();
+            } catch (err) {
+                await queryRunner.rollbackTransaction();
+            } finally {
+                await queryRunner.release();
+            }
+        }
 
 
         
