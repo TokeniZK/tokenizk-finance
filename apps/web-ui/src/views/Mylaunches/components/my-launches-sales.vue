1cf86f78016472ee47929a5a52cbe840e602a908
Yoga2015 <yoga3333@126.com>
2024-01-02 19:16:03 +0800

    Create my-launches-tokens, my-launches-sales, my-launches-airdrops components

---
+++ apps/web-ui/src/views/Mylaunches/components/my-launches-sales.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-row class="row-bg my-launches-sales">
+      <el-col :span="3"></el-col>
+      <el-col :span="18">my-launches-sales</el-col>
+      <el-col :span="3"></el-col>
+  </el-row>
+</template>
+
+<style lang="less" scoped>
+.my-launches-sales {
+  width: 100%;
+  padding-bottom: 50px;
+}
+</style>
