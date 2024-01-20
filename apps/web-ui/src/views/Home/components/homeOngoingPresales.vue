3549b8449933ad5b5cbef6feb9104b61249a20dd
Tang1903 <246968103@qq.com>
2024-01-20 14:30:43 +0800

    Optimize data acquisition operations

--- apps/web-ui/src/views/Home/components/homeOngoingPresales.vue
+++ apps/web-ui/src/views/Home/components/homeOngoingPresales.vue
@@ -1,97 +1,39 @@
 <script lang="ts" setup >
 import { ref, onMounted, reactive, computed, type ComputedRef } from 'vue'
-import { getOngoingPresaleAPI } from '@/apis/sale-api'
 import { type SaleDto, type SaleReq } from '@tokenizk/types'
 import SaleBlock from '../../../components/sale-block.vue'
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import { querySale } from '@/apis/sale-api';
+import { CircuitControllerState, useStatusStore } from '@/stores';
+import { syncLatestBlock } from '@/utils/txUtils';
+
+const { appState, showLoadingMask, setConnectedWallet, closeLoadingMask } = useStatusStore();
 
 
-const route = useRoute();
-const saleType = route.query.saleType;
+let route = useRoute();
+let saleType = ref(route.query.saleType as any as number);
 
 type SaleDtoExtend = SaleDto & { projectStatus: string, progressBarStatus: string, progressPercent: number }
 
 let fetchResult: SaleDtoExtend[] = [];
 
-// 过滤器
-const filterBy = ref('')
-const sortBy = ref('')
 
-// fitlerBy 下拉菜单 渲染所需的数据
-const filterByOptions = [
-    {
-        value: '0',
-        label: 'All Status',
-    },
-    {
-        value: '1',
-        label: 'Upcoming',
-    },
-    {
-        value: '2',
-        label: 'Ongoing',
-    },
-    {
-        value: '3',
-        label: 'Ended',
-    },
-    /*
-        {
-            value: '4',
-            label: 'Filled',
-        },
-        {
-            value: '5',
-            label: 'notFilled',
-        },
-    */
-]
-
-// sortBy 下拉菜单 渲染所需的数据
-const sortByOptions = [
-    {
-        value: '0',
-        label: 'No Sort',
-    },
-    {
-        value: '1',
-        label: 'Soft Cap',
-    },
-    {
-        value: '2',
-        label: 'Hard Cap',
-    },
-    {
-        value: '3',
-        label: 'Start time',
-    },
-    {
-        value: '4',
-        label: 'End time',
-    }
-]
-
-// 每次点击 搜索按钮 时，重置 过滤选项 和 排序选项
-const searchProjects = () => {
-    if (keyWord.value) {
-        filterBy.value = '0'
-        sortBy.value = '0'
-
-        // trigger api
-        getSearchProjects();
-    }
-}
 
 // 转换项目的状态
-const transformProjectStatus = (itmes: SaleDtoExtend[]) => {
-    let currentTime = new Date().getTime();
+const transformProjectStatus = async (itmes: SaleDtoExtend[]) => {
 
+    if (appState.latestBlockInfo!.blockchainLength == 0 || new Date().getTime() - appState.fetchLatestBlockInfoTimestamp >= 2 * 60 * 1000) {
+        appState.latestBlockInfo = (await syncLatestBlock()) ?? appState.latestBlockInfo;
+        appState.fetchLatestBlockInfoTimestamp = new Date().getTime();
+    }
+
+    const currentBlockHeight = appState.latestBlockInfo!.blockchainLength;
     itmes.forEach(item => {
-        if (item.startTimestamp > currentTime) {
+        if (item.startTimestamp > currentBlockHeight) {
             item.projectStatus = 'Upcoming'
-        } else if (item.startTimestamp <= currentTime && item.endTimestamp > currentTime) {
+        } else if (item.startTimestamp <= currentBlockHeight && item.endTimestamp > currentBlockHeight) {
             item.projectStatus = 'Ongoing'
-        } else if (item.endTimestamp < currentTime) {
+        } else if (item.endTimestamp < currentBlockHeight) {
             item.projectStatus = 'Ended'
         } else {
             item.projectStatus = 'All Status'
@@ -113,27 +55,6 @@ const calcProjectProgress = (itmes: SaleDtoExtend[]) => {
     });
 }
 
-const sortProjects = (sortByValue: string, items: SaleDtoExtend[]) => {
-    if (sortByValue == '1') {
-        items.sort((a, b) => {
-            return Number(a.softCap) - Number(b.softCap);
-        });
-    } else if (sortByValue == '2') {
-        items.sort((a, b) => {
-            return Number(b.hardCap) - Number(a.hardCap);
-        });
-    } else if (sortByValue == '3') {
-        items.sort((a, b) => {
-            return Number(b.startTimestamp) - Number(a.startTimestamp);
-        });
-    } else if (sortByValue == '4') {
-        items.sort((a, b) => {
-            return Number(b.endTimestamp) - Number(a.endTimestamp);
-        });
-    }
-
-    return items;
-}
 
 let renderSaleBlock: SaleDtoExtend[] = [];
 // 临时数据
@@ -142,234 +63,33 @@ let presaleProjects = reactive({ saleList: renderSaleBlock });
 // 获取 用户输入的关键字 进行搜索
 let keyWord = ref('')
 const getSearchProjects = async () => {
-    // const saleReq = {
-    //     saleType,
-    //     saleName: keyWord.value
-    // } as SaleReq;
-    // fetchResult = (await querySale(saleReq)) as SaleDtoExtend[];
-    // 临时数据 本尊
-    fetchResult = [
-        {
-            id: 0,
-            saleType: 1,
-            txHash: '0x333123456789',
-            status: 1,
+    const saleReq = {
+        saleType: saleType.value,
+        saleName: keyWord.value,
+        queryBriefInfo: true,
+        take: 3
+    } as any as SaleReq;
 
-            tokenName: 'TxZ',
-            tokenAddress: 'B62xxxt',
-            tokenSymbol: 'TxZ',
-
-            saleName: 'ZHI Inu 2.0',
-            saleAddress: 'B62xxs',
-
-            star: 4,
-
-            totalSaleSupply: 20,
-            currency: 'Mina',
-            feeRate: '5%',
-            saleRate: 10,
-            whitelistTreeRoot: '45678ityuioghjk',
-            whitelistMembers: 'B62xxxxw,B62xxxxwY,B62xxU',
-            totalContributorNum: 3,
-            softCap: 21,
-            hardCap: 60,
-            minimumBuy: 0.1,
-            maximumBuy: 1,
-            startTimestamp: new Date().getTime() - 10 * 60 * 60 * 1000,
-            endTimestamp: new Date().getTime() + 10 * 24 * 60 * 60 * 1000,
-            projectStatus: '',
-
-            cliffTime: 300,
-            cliffAmountRate: 3,
-            vestingPeriod: 3,
-            vestingIncrement: 10,
-            contributorsFetchFlag: 0,
-            contributorsTreeRoot: '',
-            contributorsMaintainFlag: 0,
-            totalContributedMina: 11,
-            teamName: 'Zhi Tokenizk Team',
-            logoUrl: '/src/assets/images/1.png',
-            website: 'https://tokenizk.finance/',
-            facebook: 'https://tokenizk.finance/',
-            github: 'https://tokenizk.finance/',
-            twitter: 'https://tokenizk.finance/',
-            telegram: 'https://tokenizk.finance/',
-            discord: 'https://tokenizk.finance/',
-            reddit: 'https://tokenizk.finance/',
-            description: 'The Launchpad focusing on ZK-Token for Everyone!',
-            updatedAt: 1703641015995,
-            createdAt: 1703691251595,
-
-            progressBarStatus: 'success',
-            progressPercent: 0,
-
-        },
-        {
-            id: 0,
-            saleType: 1,
-            txHash: '0x123456789',
-            status: 1,
-
-            tokenName: 'TZ',
-            tokenAddress: 'B62xxxt',
-            tokenSymbol: 'TZ',
-
-            saleName: 'Oggy Inu 2.0',
-            saleAddress: 'B62xxs',
-
-            star: 4,
-
-            totalSaleSupply: 20,
-            currency: 'Mina',
-            feeRate: '5%',
-            saleRate: 10,
-            whitelistTreeRoot: '45678ityuioghjk',
-            whitelistMembers: 'B62xxxxw,B62xxxxwY,B62xxU',
-            totalContributedMina: 80,
-            totalContributorNum: 3,
-            softCap: 11,
-            hardCap: 90,
-            minimumBuy: 0.1,
-            maximumBuy: 1,
-            startTimestamp: new Date().getTime() + 10 * 60 * 60 * 1000,
-            endTimestamp: new Date().getTime() + 10 * 24 * 60 * 60 * 1000,
-            projectStatus: '',
-
-            cliffTime: 300,
-            cliffAmountRate: 3,
-            vestingPeriod: 4,
-            vestingIncrement: 5,
-            contributorsFetchFlag: 0,
-            contributorsTreeRoot: '',
-            contributorsMaintainFlag: 0,
-
-            teamName: 'Tokenizk Team',
-            logoUrl: '/src/assets/images/2.png',
-            website: 'https://tokenizk.finance/',
-            facebook: 'https://tokenizk.finance/',
-            github: 'https://tokenizk.finance/',
-            twitter: 'https://tokenizk.finance/',
-            telegram: 'https://tokenizk.finance/',
-            discord: 'https://tokenizk.finance/',
-            reddit: 'https://tokenizk.finance/',
-            description: 'The Launchpad focusing on ZK-Token for Everyone!',
-            updatedAt: new Date().getTime(),
-            createdAt: new Date().getTime(),
-
-            progressBarStatus: 'success',
-            progressPercent: 0
-        },
-        {
-            id: 0,
-            saleType: 1,
-            txHash: '0x123456789',
-            status: 1,
-
-            tokenName: 'TZ',
-            tokenAddress: 'B62xxxt',
-            tokenSymbol: 'TZ',
-
-            saleName: 'XX Inu 2.0',
-            saleAddress: 'B62xxs',
-
-            star: 4,
-
-            totalSaleSupply: 20,
-            currency: 'Mina',
-            feeRate: '5%',
-            saleRate: 10,
-            whitelistTreeRoot: '45678ityuioghjk',
-            whitelistMembers: 'B62xxxxw,B62xxxxwY,B62xxU',
-
-            softCap: 101,
-            hardCap: 300,
-            minimumBuy: 0.1,
-            maximumBuy: 1,
-            startTimestamp: new Date().getTime() - 10 * 24 * 60 * 60 * 1000,
-            endTimestamp: new Date().getTime() - 10 * 60 * 60 * 1000,
-            projectStatus: '',
-
-            cliffTime: 300,
-            cliffAmountRate: 3,
-            vestingPeriod: 12,
-            vestingIncrement: 10,
-            contributorsFetchFlag: 0,
-            contributorsTreeRoot: '',
-            contributorsMaintainFlag: 0,
-            totalContributedMina: 100,
-            totalContributorNum: 3,
-            teamName: 'Tokenizk Team',
-            logoUrl: '/src/assets/images/3.png',
-            website: 'https://tokenizk.finance/',
-            facebook: 'https://tokenizk.finance/',
-            github: 'https://tokenizk.finance/',
-            twitter: 'https://tokenizk.finance/',
-            telegram: 'https://tokenizk.finance/',
-            discord: 'https://tokenizk.finance/',
-            reddit: 'https://tokenizk.finance/',
-            description: 'The Launchpad focusing on ZK-Token for Everyone!',
-            updatedAt: new Date().getTime(),
-            createdAt: new Date().getTime(),
-
-            progressBarStatus: 'success',
-            progressPercent: 0
-        }];
+    fetchResult = (await querySale(saleReq)) as SaleDtoExtend[];
 
     calcProjectProgress(fetchResult);
     // 转换项目状态
-    transformProjectStatus(fetchResult);
-    // sort 相等运算符会转类型、 最后一项没使用Number做转换
-    sortProjects(sortByOptions[0].value, fetchResult);
+    await transformProjectStatus(fetchResult);
 
     renderSaleBlock = fetchResult;
     presaleProjects.saleList = renderSaleBlock
 }
 
-// 根据 用户选择 filterBy的选项  过滤数据
-const triggerFilterProjects = () => {
-    // 计算项目状态
-    transformProjectStatus(fetchResult);
-
-    if (filterBy.value === '4') {
-        // TODO 
-        fetchResult.filter(item => {
-            return item.projectStatus === 'All Status'
-        })
-
-    } else if (filterBy.value === '0') {
-        renderSaleBlock = fetchResult
-    } else {
-        renderSaleBlock = fetchResult.filter(item => {
-            if (filterBy.value == '1') {
-                return item.projectStatus === 'Upcoming'
-            } else if (filterBy.value == '2') {
-                return item.projectStatus === 'Ongoing'
-            } else if (filterBy.value == '3') {
-                return item.projectStatus === 'Ended'
-            }
-        });
-    }
-
-    // sort
-    sortProjects(sortBy.value, renderSaleBlock);
-
-    presaleProjects.saleList = renderSaleBlock;
-}
-
-// 根据 用户选择 sortBy的选项 sort排序 由近到远
-const triggerSortProjects = () => {
-    // sort
-    sortProjects(sortBy.value, renderSaleBlock);
-
-    renderSaleBlock = JSON.parse(JSON.stringify(renderSaleBlock));
-    presaleProjects.saleList = renderSaleBlock;
-}
-
 
 // 组件挂载完成后执行的函数  请求数据  
-onMounted(() => {
+onMounted(async () => {
+
     getSearchProjects();
+
 })
+
+
+
 
 </script>
 
