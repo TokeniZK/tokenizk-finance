1178a276b41e1196bafba98f15300da943c4ee40
Tang1903 <246968103@qq.com>
2024-02-23 23:25:49 +0800

    init sale statistic pie chart component

---
+++ apps/web-ui/src/views/PreSales/components/sale-statistic.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+import * as echarts from 'echarts/core';
+import {
+    BarChart,
+    LineChart,
+    PieChart
+} from 'echarts/charts';
+import {
+    TitleComponent,
+    TooltipComponent,
+    GridComponent,
+    DatasetComponent,
+    TransformComponent
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import type {
+    BarSeriesOption,
+    LineSeriesOption,
+    PieSeriesOption
+} from 'echarts/charts';
+import type {
+    TitleComponentOption,
+    TooltipComponentOption,
+    GridComponentOption,
+    DatasetComponentOption,
+} from 'echarts/components';
+import type {
+    ComposeOption,
+} from 'echarts/core';
+
+type ECOption = ComposeOption<
+    | BarSeriesOption
+    | LineSeriesOption
+    | PieSeriesOption
+    | TitleComponentOption
+    | TooltipComponentOption
+    | GridComponentOption
+    | DatasetComponentOption
+>;
+
+echarts.use([
+    TitleComponent,
+    TooltipComponent,
+    GridComponent,
+    DatasetComponent,
+    TransformComponent,
+    BarChart,
+    LineChart,
+    PieChart,
+    LabelLayout,
+    UniversalTransition,
+    CanvasRenderer
+]);
+
+const props = defineProps<{
+    dataArr: { value: number, name: string }[]
+}>();
+
+const option: ECOption = {
+    series: [
+        {
+            type: 'pie',
+            data: props.dataArr
+        }
+    ]
+};
+
+var myChart = echarts.init(document.getElementById('sale-statistic'));
+myChart.setOption(option);
+
+</script>
+<template>
+    <div id="sale-statistic">
+
+    </div>
+</template>
