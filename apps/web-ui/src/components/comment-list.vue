1a14f3308e31c9728ec428d1f95834ccf0a94515
Tang1903 <246968103@qq.com>
2024-03-06 20:39:17 +0800

    Add the comment interface and optimize the "comment-list" component

--- apps/web-ui/src/components/comment-list.vue
+++ apps/web-ui/src/components/comment-list.vue
@@ -1,31 +1,41 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import CommentForm from './CommentForm.vue';
-
-interface Comment {
-  author: string;
-  text: string;
-}
-
-const comments = ref<Comment[]>([]);
-
-function addComment(newComment: Comment) {
-  comments.value.push(newComment);
-}
-
-function deleteComment(index: number) {
-  comments.value.splice(index, 1);
-}
+import { onMounted, ref } from 'vue'
+import {type Comment} from '@/models/comment' 
+  
+const comments = ref<Comment[]>([]);  
+  
+// 模拟从后端获取评论数据  
+const fetchComments = async () => {  
+  // 使用 axios 或其他 HTTP 客户端从后端获取数据  
+  // 假设返回的数据格式是 Comment[]  
+  const response = await fetch('/api/comments');  
+  const data = await response.json();  
+  comments.value = data;  
+};  
+  
+// 在组件挂载时获取评论数据  
+onMounted(fetchComments);  
 
 </script>
 
-<template>
-  <div>
-    <div v-for="(comment, index) in comments" :key="index" class="comment">
-      <h3>{{ comment.author }}</h3>
-      <p>{{ comment.text }}</p>
-      <button @click="deleteComment(index)">Delete</button>
-    </div>
-    <CommentForm @submitComment="addComment" />
-  </div>
+<template>  
+  <div class="comment-list">  
+    <div v-for="comment in comments" :key="comment.id" class="comment-item">  
+      <h3>{{ comment.author }}</h3>  
+      <p>{{ comment.text }}</p>  
+      <span>{{ comment.timestamp.toLocaleString() }}</span>  
+    </div>  
+  </div>  
 </template>
+
+<style scoped>  
+.comment-list {  
+  /* 样式美化 */  
+}  
+  
+.comment-item {  
+  /* 样式美化 */  
+}  
+</style>
+
+
