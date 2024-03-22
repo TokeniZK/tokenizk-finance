f7d642b0fdfed9308d7f3ef09e08377c6e30a10b
Tang1903 <246968103@qq.com>
2024-03-22 23:32:52 +0800

    add submit comment endpoint

---
+++ apps/api-gateway/src/app/routes/comment/submit-comment.ts
@@ -0,0 +1,131 @@
+import httpCodes from "@inip/http-codes"
+import { FastifyPlugin } from "fastify"
+import { BaseResponse, ClientProveTaskDto, ClientProveTaskDtoSchema, ProofTaskDto, ProofTaskType } from '@tokenizk/types'
+import { RequestHandler } from '@/lib/types'
+import fs from 'fs'
+import { getConnection } from "typeorm"
+import { getLogger } from "@/lib/logUtils"
+import { ClientProveTask, UserTokenAirdrop, Comment, Airdrop,Sale } from "@tokenizk/entities"
+import { $axiosCore, $axiosProofGen } from "@/lib/api"
+import { ClientProofReqType, CommentDto } from "@tokenizk/types"
+import { fetchLastBlock } from 'o1js';
+
+const logger = getLogger('submitCommentsByAddr');
+
+export const submitComment: FastifyPlugin = async function (
+    instance,
+    options,
+    done
+): Promise<void> {
+    instance.route({
+        method: "POST",
+        url: "/submit-comments",
+        schema,
+        handler
+    })
+}
+
+const handler: RequestHandler<CommentDto, null> = async function (
+    req,
+    res
+): Promise<BaseResponse<Array<CommentDto>>> {
+    const dto = req.body;
+
+    try {
+        const connection = getConnection();
+        const commentRepo = connection.getRepository(Comment);
+
+        // check if parent comment existed
+        if (dto.parentCommentId) {
+            const parentComment = await commentRepo.findOne({
+                where: {
+                    id: dto.parentCommentId
+                }
+            });
+
+            if (parentComment) {
+                // submit comment
+                const comment = new Comment();
+                Object.assign(comment, CommentDto);
+                await commentRepo.save(comment);
+                return {
+                    code: 0,
+                    data: null,
+                    msg: ''
+                }
+            } else {
+                return {
+                    code: 1,
+                    data: null,
+                    msg: 'parent comment not found'
+                }
+            }
+        } else {
+            // check if project exited
+            if(dto.projectType === 4){// airdrop
+                const airdropRepo = connection.getRepository(Airdrop)
+                const airdrop = await airdropRepo.findOne({
+                    where: {
+                        airdropAddress: dto.projectAddress,
+                        status: 1
+                    }
+                })
+                if(!airdrop){
+                    return {
+                        code: 1,
+                        data: null,
+                        msg: 'project not found'
+                    }
+                }
+            } else {//sale
+                const saleRepo = connection.getRepository(Sale)
+                const sale = await saleRepo.findOne({
+                    where: {
+                        saleAddress: dto.projectAddress,
+                        status: 1
+                    }
+                })
+                if (!sale) {
+                    return {
+                        code: 1,
+                        data: null,
+                        msg: 'project not found'
+                    }
+                }
+            }
+        }
+
+    } catch (err) {
+        logger.error(err);
+        console.error(err);
+    }
+}
+
+const schema = {
+    description: 'submit comments',
+    tags: ["Comments"],
+    body: {
+        type: CommentDtoSchema.type,
+        properties: CommentDtoSchema.properties
+    },
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                code: {
+                    type: 'number',
+                },
+                data: {
+                    type: 'array',
+                    items: {
+                        type: CommentDtoSchema.type,
+                        properties: CommentDtoSchema.properties
+                    }
+                },
+                msg: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
